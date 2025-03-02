b815819c0a3149b18ff2dc6d9f3ea3d1d18ef0cf
asksamalam <asksamalam@gmail.com>
2025-03-02 12:43:16 +0530

    Initial Commit

---
+++ src/main/java/in/wowpdf/Wowpdf/WowpdfApplication.java
@@ -0,0 +1,13 @@
+package in.wowpdf.Wowpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowpdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowpdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/wowpdf/Wowpdf/WowpdfApplicationTests.java
@@ -0,0 +1,13 @@
+package in.wowpdf.Wowpdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowpdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
